aaef0c4095f79266cabb82582ac8d7697481aa52
Butler, Walt <Walt.Butler@sirva.com>
2019-10-08 12:50:40 -0400

    Add project files.

---
+++ ModelDemo/ModelDemo.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using ModelDemo.ServiceModel;
+
+namespace ModelDemo.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
